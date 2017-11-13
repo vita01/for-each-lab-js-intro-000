a149b89943bca4013b85cd241396599c120bfb98
Victoria <vitalina27@gmail.com>
2017-11-13 14:13:16 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,14 @@
+function iterativeLog(arr){
+arr.forEach(function(item, i, arr) {
+  console.log('${i}: ${item}');
+  return i+' : '+ item;
+//  alert( i + ": " + item + " (массив:" + arr + ")" );
+});
+
+}
+
+function iterate(callback){
+
+
+}
+function doToArray(arr,callback)
